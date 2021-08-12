c4b340a038941c42aec7715ec56267b89ec3da73
ASUS <yamni.zero@gmail.com>
2021-08-12 12:49:49 +0200

    ad

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:car_app/Widgest/Divider.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainScreen extends StatefulWidget
@@ -17,6 +18,27 @@ class _MainScreenState extends State<MainScreen>
 {
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   late GoogleMapController newGoogleMapController;
+
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  //geolocator
+  late Position currentPosition;
+  var geoLocator = Geolocator();
+  double bottomPaddingOfMap = 0;
+
+
+  void locatePosition() async
+  {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    currentPosition = position;
+
+    LatLng latLatPosition = LatLng(position.altitude, position.longitude);
+
+    CameraPosition cameraPosition = new  CameraPosition(target: latLatPosition,zoom: 14);
+    newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+  //geolocator
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -25,42 +47,134 @@ class _MainScreenState extends State<MainScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text("Main Screen"),
+      ),
+      drawer: Container(
+        color: Colors.white,
+        width: 255.0,
+        child: Drawer(
+          child: ListView(
+            children:
+            [
+              //Drawer Header
+              Container(
+                height: 165.0,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Row(
+                    children:
+                    [
+                      Image.asset("images/user.png",height: 80.0,width: 80.0,),
+                      SizedBox(width: 16.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Profile Name",style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold),),
+                          SizedBox(height: 6.0,),
+                          Text("Visit profile"),
+
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              DividerWidget(),
+              SizedBox(height: 12.0,),
+              //Drawer Body Container
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text("History",style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Visit profile",style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("About",style: TextStyle(fontSize: 15.0),),
+              ),
+            ],
+          ),
+        ),
       ),
       body: Stack(
         children:
         [
           GoogleMap(
+            padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
             mapType: MapType.normal,
             myLocationButtonEnabled: true,
             initialCameraPosition: _kGooglePlex,
+            myLocationEnabled: true,
+            zoomGesturesEnabled: true,
+            zoomControlsEnabled: true,
             onMapCreated: (GoogleMapController controller)
             {
               _controllerGoogleMap.complete(controller);
               newGoogleMapController = controller;
+
+              setState(() {
+                bottomPaddingOfMap =300.0;
+              });
+
+              locatePosition();
             },
           ),
+
+          //HamburgerButton for Drawer
+          Positioned(
+            top: 45.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap: ()
+              {
+                scaffoldKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(22.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 6.0,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7,0.7),
+                      )
+                    ]
+                ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.menu,color: Colors.black,),
+                  radius: 20.0,
+                ),
+              ),
+            ),
+          ),
+
           Positioned(
             left: 0.0,
             right: 0.0,
             bottom: 0.0,
             child: Container(
-              height: 320.0,
+              height: 280.0,
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(18.0),
-                  topRight: Radius.circular(18.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 16.0,
-                    spreadRadius: 0.5,
-                    offset: Offset(0.7,0.7),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0),
                   ),
-                ]
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 16.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7,0.7),
+                    ),
+                  ]
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
@@ -70,11 +184,11 @@ class _MainScreenState extends State<MainScreen>
                   [
                     SizedBox(height: 6.0,),
                     Text(
-                        "Hi there",
+                      "Hi there",
                       style: TextStyle(fontSize: 12.0),
                     ),
                     Text(
-                        "Where to?",
+                      "Where to?",
                       style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 20.0,),
@@ -116,8 +230,8 @@ class _MainScreenState extends State<MainScreen>
                             Text("Add Home"),
                             SizedBox(height: 4.0,),
                             Text(
-                                "Your living home address",
-                            style: TextStyle(color: Colors.black54,fontSize: 12.0),
+                              "Your living home address",
+                              style: TextStyle(color: Colors.black54,fontSize: 12.0),
                             ),
                           ],
                         ),
